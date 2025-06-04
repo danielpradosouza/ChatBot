d03c06ca9e2da0cb9341a2c7c92d800a6a1a5d35
danielpradosouza <79987537+danielpradosouza@users.noreply.github.com>
2025-06-04 09:42:31 -0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -11,15 +11,16 @@ else:
         presensa=int(input("o atendimento sera presincial ou não:"
                             "digite 1 para prencial e 2 para virtual"))
         if presensa==1:   
-                ambiante=(input("sua empresa poossui auditorio?: digite "
-                                "1 para sim e 2 para não")) 
+                ambiante=(int(input("sua empresa poossui auditorio?: digite "
+                                "1 para sim e 2 para não"))) 
                 if ambiante==1:
                      print("Estou te colocando para "
-                         "  entrar em contato com a Patricia")
+                         " entrar em contato com a Patricia")
                 else:
                      print("Tente arrumar um local e" 
                            " entre em contato com a Patricia")
+                
              
         else:
             print("Estou te colocando para "
-                         "  entrar em contato com a Patricia")
+                         " entrar em contato com a Patricia")
