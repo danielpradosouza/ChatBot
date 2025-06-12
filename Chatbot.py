b5d463d6bd63f28e54f15c8f4fea5b29fdad06d0
danielpradosouza <79987537+danielpradosouza@users.noreply.github.com>
2025-06-12 09:18:25 -0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -17,21 +17,23 @@ if tipo_pess==1:
                         "1 para em grupo,2 para casal" 
                         ",3 para individual "))
         if tipo_At==1:
-            presensa=int(input("o atendimento sera presincial ou não:"
-                                "digite 1 para prencial e 2 para virtual"))
-            if presensa==1:   
-                ambiante=(int(input("sua empresa poossui auditorio?: digite "
-                                "1 para sim e 2 para não"))) 
-                if ambiante==1:
-                    print("Estou te colocando para "
-                        " entrar em contato com a Patricia")
-                else:
-                    print("Tente arrumar um local e" 
-                        " entre em contato com a Patricia")                             
-            else:
-                print("Estou te colocando para "
-                        " entrar em contato com a Patricia")
+           qunat_pess=int(input("Para quantas pessoas será o atendimento?"))
+           Apli=int(input("Você prefere usar algum aplicativo específico? Algumas opções: Microsoft"
+                          "Teams, Google Meet ou WhatsApp (chamada de vídeo)"
+                          "Digite 1 para microsoft Teams, 2 para google meet e 3 para whatsApp"))
+           atendi=int(input("Como você gostaria de organizar o atendimento?"
+                            "digite 1 para uma sessão específica, 2 para Acompanhamento semanal"
+                            "e 3 para Acompanhamento mensal"))
+           print("Assim que possível a psicóloga Patrícia Marques entrará em contato com você"
+                 "para realizar o agendamento. Sua equipe desde já agradece a preferência!")
         if tipo_At==2:
+            nome_conj=input("Digite o nome do seu Conjuge")
+            atendi=int(input("Como você gostaria de organizar o atendimento?"
+                            "digite 1 para uma sessão específica, 2 para Acompanhamento semanal"
+                            "e 3 para Acompanhamento mensal"))
+            print("Assim que possível a psicóloga Patrícia Marques entrará em contato com você"
+                 "para realizar o agendamento. Sua equipe desde já agradece a preferência!")
+        if tipo_At==3:
             presensa1=(int(input("o atendimento sera presincial ou não:"
                                 "digite 1 para prencial e 2 para virtual")))
             if presensa1==1:
@@ -46,7 +48,4 @@ if tipo_pess==1:
             else:
                 print("Estou te colocando para "
                         " entrar em contato com a Patricia")
-        if tipo_At==3:
-            nome_conj=input("Digite o nome do seu Conjuge")
-            print("Estou te colocando para entrar em contato com a Patricia pois" 
-                "o atendimento será somente virtual")
+        
