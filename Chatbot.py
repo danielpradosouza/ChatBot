8d48ef693fb6f2e43628d8ab910e7df2ecf9c5d5
danielpradosouza <79987537+danielpradosouza@users.noreply.github.com>
2025-06-16 11:15:01 -0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,5 +1,5 @@
-tipo_pess=int(input("Olá! Para melhor te atender, você é uma" 
-                    "pessoa física ou uma empresa? digite 1" 
+tipo_pess=int(input("Olá! Para melhor te atender, você é uma " 
+                    "pessoa física ou uma empresa? digite 1 " 
                     "para pessoa fisica e 2 para pessoa juridica"))
 if tipo_pess==1:
     nome=input("Digite seu nome:")
@@ -14,28 +14,28 @@ if tipo_pess==1:
         idade_atual=True
     if idade_atual==True:
         tipo_At=int(input("qual tipo de atendimento voce deseja:" 
-                        "1 para em grupo,2 para casal" 
-                        ",3 para individual "))
+                        "1 para em grupo,2 para casal " 
+                        "e 3 para individual "))
         if tipo_At==1:
            qunat_pess=int(input("Para quantas pessoas será o atendimento?"))
-           Apli=int(input("Você prefere usar algum aplicativo específico? Algumas opções: Microsoft"
-                          "Teams, Google Meet ou WhatsApp (chamada de vídeo)"
+           Apli=int(input("Você prefere usar algum aplicativo específico? Algumas opções: Microsoft "
+                          "Teams, Google Meet ou WhatsApp (chamada de vídeo) "
                           "Digite 1 para microsoft Teams, 2 para google meet e 3 para whatsApp"))
            atendi=int(input("Como você gostaria de organizar o atendimento?"
-                            "digite 1 para uma sessão específica, 2 para Acompanhamento semanal"
+                            "digite 1 para uma sessão específica, 2 para Acompanhamento semanal "
                             "e 3 para Acompanhamento mensal"))
-           print("Assim que possível a psicóloga Patrícia Marques entrará em contato com você"
+           print("Assim que possível a psicóloga Patrícia Marques entrará em contato com você "
                  "para realizar o agendamento. Sua equipe desde já agradece a preferência!")
         if tipo_At==2:
             nome_conj=input("Digite o nome do seu Conjuge")
             atendi=int(input("Como você gostaria de organizar o atendimento?"
-                            "digite 1 para uma sessão específica, 2 para Acompanhamento semanal"
+                            "digite 1 para uma sessão específica, 2 para Acompanhamento semanal "
                             "e 3 para Acompanhamento mensal"))
-            print("Assim que possível a psicóloga Patrícia Marques entrará em contato com você"
+            print("Assim que possível a psicóloga Patrícia Marques entrará em contato com você "
                  "para realizar o agendamento. Sua equipe desde já agradece a preferência!")
         if tipo_At==3:
            atendi=int(input("Como você gostaria de organizar o atendimento?"
-                            "digite 1 para uma sessão específica, 2 para Acompanhamento semanal"
+                            "digite 1 para uma sessão específica, 2 para Acompanhamento semanal "
                             "e 3 para Acompanhamento mensal"))
-           print("Assim que possível a psicóloga Patrícia Marques entrará em contato com você"
+           print("Assim que possível a psicóloga Patrícia Marques entrará em contato com você "
                  "para realizar o agendamento. Sua equipe desde já agradece a preferência!")
